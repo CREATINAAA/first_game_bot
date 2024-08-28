e575040bf74184b1bb63a6336d1388f3a27c1c14
Daniil <samtesred@gmail.com>
2024-08-28 12:20:13 +0300

    first commit

--- src/app/settings.py
+++ src/app/settings.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-BASE_DIR = Path(__name__).resolve().parent.parent.parent
-
+BASE_DIR = Path(__name__).resolve()
+print(BASE_DIR)
 load_dotenv(os.path.join(BASE_DIR / '.env'))
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
